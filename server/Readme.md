2b9ec00849c821ee22496974f0dc352f6d984f48
Aaryan10-sudo <lazyfox916@gmail.com>
2025-03-13 12:39:40 +0545

    Server setup

---
+++ server/Readme.md
@@ -0,0 +1,3 @@
+# Hello from Chasing Horizon Server
+
+This server is built in top of express.js
---
+++ server/src/app.js
@@ -0,0 +1,42 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+app.get("/health", (req, res) => {
+  res.send("Server is working fine");
+});
+app.get("/version", (req, res) => {
+  res.send("Chasing Horizon Version 2.0");
+});
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
