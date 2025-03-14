f9a5442e428786ba4d0dbaff541c9593010cacc6
Aaryan10-sudo <lazyfox916@gmail.com>
2025-03-14 10:48:00 +0545

    Server setup

--- server/src/app.js
+++ server/src/app.js
@@ -6,24 +6,33 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var connectMongo = require("./config/db/connectMongoose");
+var { defaultLimiter } = require("./middleware/express-limit/express-limit");
+const errorHandler = require("./middleware/error/errorHandler");
 
 var app = express();
 
+connectMongo();
+errorHandler();
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
+
 app.get("/health", (req, res) => {
   res.send("Server is working fine");
 });
+
 app.get("/version", (req, res) => {
-  res.send("Chasing Horizon Version 2.0");
+  res.send("Chasing Horizon Version 1.0.0");
 });
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/", defaultLimiter, indexRouter);
 app.use("/users", usersRouter);
 
 app.use(function (req, res, next) {
